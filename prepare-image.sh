8ba099f06b46e6edbcbd1c184b9c857afad0332e
Riccardo Pittau <elfosardo@gmail.com>
2021-05-25 16:28:05 +0200

    Switch to Centos 8 Stream base image

--- prepare-image.sh
+++ prepare-image.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 dnf install -y python3 python3-requests epel-release 'dnf-command(config-manager)'
 dnf config-manager --set-disabled epel
-curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo --no-stream
+curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
 xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/${PKGS_LIST}
 if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
